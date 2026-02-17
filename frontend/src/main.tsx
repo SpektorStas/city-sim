b49f46437b9f6a636b832da99eacdf515c096b05
TheRollingDalek <spektorstas@gmail.com>
2026-02-17 11:50:16 +0300

    main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./app/App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
